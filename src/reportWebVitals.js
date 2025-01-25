9e3dbba10855aa8b0ac0ddcde73d15c2b18b3c94
YrnCal <calvinitharakimanikihuyu@gmail.com>
2025-01-25 18:20:44 +0300

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,19 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        // Call each of the web vitals metric functions
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      })
+      .catch((error) => {
+        console.error('Error loading web-vitals:', error);
+      });
+  } else {
+    console.warn('No callback function provided for web vitals.');
   }
 };
-
 export default reportWebVitals;
